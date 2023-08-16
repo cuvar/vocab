cea97b2be76f2a30fc690e0eddf4ae5dd94c5de5
cuvar <luca-mueller@gmx.net>
2023-08-16 17:52:53 +0200

    fix: tapbar rework

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { type NextPage } from "next";
 import Head from "next/head";
 
 import { api } from "../utils/api";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { signOut, useSession } from "next-auth/react";
 import LogoutScreen from "../comp/LogoutScreen";
 import Generator from "../comp/Generator";
@@ -20,6 +20,7 @@ const TABS: Record<Tab, TabDisplay> = {
 
 const Home: NextPage = () => {
   const [tab, setTab] = useState<Tab>("list");
+  const tabContainerRef = useRef(null);
   // const initDB = api.word.initDB.useMutation();
 
   const { data } = useSession();
@@ -27,9 +28,16 @@ const Home: NextPage = () => {
     return <LogoutScreen />;
   }
 
-  function switchTab(clicked: Tab) {
+  function switchTab(ev: React.MouseEvent<HTMLButtonElement>, clicked: Tab) {
+    if (tabContainerRef.current) {
+      // @ts-ignore
+      [...tabContainerRef.current.children].forEach((e: any) => {
+        e.classList.remove("tab-active");
+      });
+      // @ts-ignore
+      ev.target.classList.add("tab-active");
+    }
     setTab(clicked);
-    console.log();
   }
 
   return (
@@ -52,20 +60,22 @@ const Home: NextPage = () => {
           </button>
         </div>
         <div className="flex w-full flex-col items-center ">
-          <div className="flex space-x-8">
+          <div className="tabs tabs-boxed" ref={tabContainerRef}>
             {(Object.keys(TABS) as Tab[]).map((e) => {
               const custom = tab === e ? "font-bold" : "";
               return (
                 <button
                   key={e}
-                  onClick={() => switchTab(e)}
-                  className={`${custom} text-xl `}
+                  onClick={(ev) => switchTab(ev, e)}
+                  className={`tab`}
                 >
                   {TABS[e]}
                 </button>
               );
             })}
           </div>
+
+          <div className="flex space-x-8"></div>
           <div className="my-16 flex w-full justify-center">
             {tab == "generator" && <Generator />}
             {tab == "list" && <Learned />}
