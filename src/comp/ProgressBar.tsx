f7b3a3a3c69ce23a5057b1be5f308b1793cf45fd
cuvar <luca-mueller@gmx.net>
2023-09-02 23:38:49 +0200

    feat: add counter to progress bar

--- src/comp/ProgressBar.tsx
+++ src/comp/ProgressBar.tsx
@@ -7,19 +7,23 @@ type Props = {
 export default function ProgressBar(props: Props) {
   const barRef = useRef<HTMLDivElement>(null);
   const progress = (props.current / props.max) * 100;
-  console.log(progress);
 
   if (barRef.current) {
     barRef.current.style.width = `${progress}%`;
   }
 
   return (
-    <div className="relative h-2 w-full">
-      <div className="absolute h-full w-full rounded-full bg-white"></div>
-      <div
-        ref={barRef}
-        className={`absolute left-0 h-full rounded-full bg-primary`}
-      ></div>
+    <div className="flex w-full flex-col items-center space-y-2">
+      <div className="relative h-2 w-full">
+        <div className="absolute h-full w-full rounded-full bg-white"></div>
+        <div
+          ref={barRef}
+          className={`absolute left-0 h-full rounded-full bg-primary`}
+        ></div>
+      </div>
+      <div className="w-full text-center">
+        {props.current} / {props.max}
+      </div>
     </div>
   );
 }
