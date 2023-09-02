fcb51d8513adac489305a7bd4bfcd4c73f8d4689
cuvar <luca-mueller@gmx.net>
2023-09-02 16:19:09 +0200

    fix: pwa background color

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <Head>
-        <meta name="theme-color" content="#641ae6" />
+        <meta name="theme-color" content="#1d232ae6" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
