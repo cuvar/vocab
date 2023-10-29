51585efbb958884878c42b5d2433d92db4313837
cuvar <luca-mueller@gmx.net>
2023-10-29 13:08:38 +0100

    feat: responsive design for tags site

--- src/comp/TagItem.tsx
+++ src/comp/TagItem.tsx
@@ -2,7 +2,7 @@ import { useAtom } from "jotai";
 import { useState } from "react";
 import { toastTextAtom, toastTypeAtom } from "../server/store";
 import { api } from "../utils/api";
-import { checkedIcon, penIcon } from "../utils/icons";
+import { checkedIcon, crossIcon, penIcon } from "../utils/icons";
 
 type Props = {
   id?: string;
@@ -73,35 +73,41 @@ export default function TagItem(props: Props) {
     }
   }
 
+  function handleDelete() {
+    confirm("deleting?");
+  }
+
   return (
-    <div className="border-base-500 flex w-full space-x-4 border-b px-2 py-4">
-      <div className="form-control w-1/3 max-w-xs">
-        <label className="label">
-          <span className="label-text">Name</span>
-        </label>
-        <input
-          type="text"
-          placeholder="Type here"
-          className="input-bordered input w-full max-w-xs"
-          disabled={!editMode}
-          value={nameInput}
-          onChange={(e) => setNameInput(e.target.value)}
-        />
+    <div className="border-base-500 flex w-full flex-col space-y-4 space-x-0 border-b px-2 py-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+      <div className="flex space-x-4">
+        <div className="form-control w-1/4 max-w-xs">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input-bordered input w-full max-w-xs"
+            disabled={!editMode}
+            value={nameInput}
+            onChange={(e) => setNameInput(e.target.value)}
+          />
+        </div>
+        <div className="form-control w-3/4 max-w-xs">
+          <label className="label">
+            <span className="label-text">Description</span>
+          </label>
+          <textarea
+            placeholder="Type here"
+            className="textarea-bordered textarea w-full max-w-xs"
+            disabled={!editMode}
+            value={descInput}
+            rows={1}
+            onChange={(e) => setDescInput(e.target.value)}
+          />
+        </div>
       </div>
-      <div className="form-control w-2/3 max-w-xs">
-        <label className="label">
-          <span className="label-text">Description</span>
-        </label>
-        <textarea
-          placeholder="Type here"
-          className="textarea-bordered textarea w-full max-w-xs"
-          disabled={!editMode}
-          value={descInput}
-          rows={1}
-          onChange={(e) => setDescInput(e.target.value)}
-        />
-      </div>
-      <div className="flex flex-col justify-end">
+      <div className="flex items-end justify-end space-x-4 sm:justify-end">
         {editMode ? (
           <button
             className="btn-secondary btn align-bottom"
@@ -117,6 +123,9 @@ export default function TagItem(props: Props) {
             {penIcon}
           </button>
         )}
+        <button className="btn-error btn align-bottom" onClick={handleDelete}>
+          {crossIcon}
+        </button>
       </div>
     </div>
   );
