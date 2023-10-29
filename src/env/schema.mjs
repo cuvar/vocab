3808921bc4deb582e53e2502da3c9c1bf49c71e4
cuvar <luca-mueller@gmx.net>
2023-10-29 14:41:38 +0100

    refactor: outsource icons

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -43,7 +43,8 @@ export const serverEnv = {
  * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 export const clientSchema = z.object({
-  // NEXT_PUBLIC_CLIENTVAR: z.string(),
+  NEXT_PUBLIC_NATIVE_ICON: z.string(),
+  NEXT_PUBLIC_TRANSLATION_ICON: z.string(),
 });
 
 /**
@@ -53,5 +54,6 @@ export const clientSchema = z.object({
  * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
  */
 export const clientEnv = {
-  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  NEXT_PUBLIC_NATIVE_ICON: process.env.NEXT_PUBLIC_NATIVE_ICON,
+  NEXT_PUBLIC_TRANSLATION_ICON: process.env.NEXT_PUBLIC_TRANSLATION_ICON,
 };
