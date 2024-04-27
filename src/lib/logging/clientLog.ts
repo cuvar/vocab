90419f78332e8074541b3350894345fdf0475be1
cuvar <luca@cuvar.dev>
2024-04-27 09:11:19 +0200

    feat: logging module

---
+++ src/lib/logging/clientLog.ts
@@ -0,0 +1,30 @@
+import { env } from "~/env.mjs";
+
+type Logtype = "log" | "info" | "warn" | "error";
+
+/**
+ * Logs information to the console
+ * @param msg Message to log
+ * @param type Type of log
+ */
+export default function Log(msg: unknown, type: Logtype = "log") {
+  if (
+    process.env.NODE_ENV !== "production" ||
+    env.NEXT_PUBLIC_DEBUG == "true"
+  ) {
+    switch (type) {
+      case "log":
+        console.log(msg);
+        break;
+      case "info":
+        console.info(msg);
+        break;
+      case "warn":
+        console.warn(msg);
+        break;
+      case "error":
+        console.error(msg);
+        break;
+    }
+  }
+}
