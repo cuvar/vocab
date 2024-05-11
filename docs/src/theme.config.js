e934776135354cf9dfa0c893ad9c630697329214
cuvar <luca@cuvar.dev>
2024-05-11 17:01:34 +0200

    fix: general setup

--- docs/src/theme.config.js
+++ docs/src/theme.config.js
@@ -1,7 +1,7 @@
 export default {
-  logo: <span>My Nextra Documentation</span>,
+  logo: <div className="flex space-x-2 items-center font-bold"><img src="/icon.png" height={50} /><span>vocab</span></div>,
   project: {
-    link: 'https://github.com/shuding/nextra'
+    link: 'https://github.com/cuvar/vocab'
   }
   // ... other theme options
 }
