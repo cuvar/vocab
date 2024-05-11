c75cb7e10473b7280800a8291535377081bff5ee
cuvar <luca@cuvar.dev>
2024-05-11 18:29:33 +0200

    fix(ui): simple landing page without demo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,7 +23,10 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="vocab" />
-        <meta name="description" content="Simple vocabulary app" />
+        <meta
+          name="description"
+          content="A simple flash cards app for learning new stuff"
+        />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <link rel="apple-touch-icon" sizes="192x192" href="/icon-192x192.png" />
