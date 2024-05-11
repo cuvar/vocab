bdadae32d1cf91118a06166913404706409ce936
Luca Müller <49287189+cuvar@users.noreply.github.com>
2024-05-11 18:33:13 +0200

    fix(ui): simple landing page without demo (#57)
    * fix(ui): simple landing page without demo
    
    * fix: add favicon for docs

--- src/sites/LogoutScreen.tsx
+++ src/sites/LogoutScreen.tsx
@@ -1,4 +1,5 @@
 import { signIn } from "next-auth/react";
+import Link from "next/link";
 
 export default function LogoutScreen() {
   function handleLogin() {
@@ -6,18 +7,43 @@ export default function LogoutScreen() {
       await signIn();
     })();
   }
-  
+
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <div className="flex flex-col space-y-4">
-        <h1 className="text-xl font-bold">Not signed in</h1>
-        <button
-          className="hover:underline active:text-blue-500"
-          onClick={handleLogin}
-        >
+    <div className="flex h-full min-h-screen flex-col justify-between text-gray-100 ">
+      <div className="flex justify-end px-4 py-4">
+        <button className="btn-ghost btn" onClick={handleLogin}>
           Sign in
         </button>
       </div>
+      <main className="bg-panal-500 flex h-full flex-col items-center px-5">
+        <div className="bg-panal-700 flex h-screen w-screen flex-col items-center justify-center sm:flex-row">
+          {/* left */}
+          <div className="flex h-full w-full flex-col justify-center space-y-10 px-10 ">
+            <h1 className="text-3xl font-semibold sm:text-4xl md:text-5xl">
+              vocab
+            </h1>
+            <h2 className="text-xl font-semibold sm:text-2xl md:text-3xl">
+              A self-hosted flash cards app for learning new stuff
+            </h2>
+            <div className="flex space-x-4">
+              <Link
+                href={"https://github.com/cuvar/vocab"}
+                className="underline hover:text-secondary"
+                target="_blank"
+              >
+                GitHub
+              </Link>
+              <Link
+                href={"https://vocab-docs.vercel.app"}
+                className="underline hover:text-secondary"
+                target="_blank"
+              >
+                Docs
+              </Link>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
