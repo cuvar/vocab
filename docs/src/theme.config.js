359828c894b1294061ec6710f3b1a7658e48fdb0
cuvar <luca@cuvar.dev>
2024-05-11 17:33:52 +0200

    feat: seo and footer

--- docs/src/theme.config.js
+++ docs/src/theme.config.js
@@ -1,7 +1,31 @@
+// see https://nextra.site/docs/docs-theme/theme-configuration
 export default {
+  docsRepositoryBase: 'https://github.com/cuvar/vocab/tree/main/docs',
   logo: <div className="flex space-x-2 items-center font-bold"><img src="/icon.png" height={50} /><span>vocab</span></div>,
   project: {
     link: 'https://github.com/cuvar/vocab'
+  },
+  footer: {
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{' '}
+        <a href="https://nextra.site" target="_blank">
+          cuvar
+        </a>
+        .
+      </span>
+    )
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="vocab" />
+      <meta property="og:description" content="A simple flash cards app for learning stuff" />
+    </>
+  ),
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s - vocab'
+    }
   }
-  // ... other theme options
 }
